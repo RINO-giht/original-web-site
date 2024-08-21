0e02cea53c03e99dc4a55e1f3027f196eda73c26
RINO-giht <birdnisizawa@icloud.com>
2024-08-22 00:05:39 +0800

    0821

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $('.menu-icon').click(function() {
-    $('.nav-menu').toggleClass('active');
-  });
-});
+// $(document).ready(function() {
+//   $('.menu-icon').click(function() {
+//     $('.nav-menu').toggleClass('active');
+//   });
+// });
