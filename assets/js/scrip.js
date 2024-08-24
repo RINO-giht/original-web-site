5ae2c60db35afe6d4e2464e6d448515240c8fa8f
RINO-giht <birdnisizawa@icloud.com>
2024-08-24 16:39:59 +0800

    変更しました

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -12,3 +12,23 @@ document.querySelectorAll('#menu-footer a').forEach(anchor => {
         }
     });
 });
+
+let slideIndex = 0;
+autoShowSlides();
+
+function autoShowSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(autoShowSlides, 4000); // 4秒ごとにスライドが切り替わる
+}
